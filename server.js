7e8a8308e087c7df92131ff3cbdde2479a32e60b
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-12 19:26:47 -0400

    updated server routes

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ mongoose.connect(
 
 // routes
 app.use(require('./routes/api.js'));
-app.use(require('./routes/htmlRoutes'));
+app.use(require('./routes/htmlRoutes.js'));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
