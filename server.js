b2afa53852270e329e5424b026ba2849e193c891
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-12 19:09:21 -0400

    updated the server with html route

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ mongoose.connect(
 
 // routes
 app.use(require('./routes/api.js'));
+app.use(require('./routes/htmlRoutes'));
 
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
