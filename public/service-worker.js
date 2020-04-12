2d2cab6ffd50ffd324e8b0040fd35e69fca5fd8f
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-12 00:27:22 -0400

    service worker updated for cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,31 +1,19 @@
-const FILES_TO_CACHE = [ '/', 'index.html', 'style.css', 'index.js' ];
+const FILES_TO_CACHE = [ '/', '/index.html', '/style.css', '/index.js' ];
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
-console.log(FILES_TO_CACHE);
-
 self.addEventListener('install', (evt) => {
 	evt.waitUntil(
-		caches
-			.open(PRECACHE)
-			.then((cache) => cache.addAll(FILES_TO_CACHE))
-			.then(self.skipWaiting())
+		caches.open(PRECACHE).then((cache) => {
+			cache.addAll(FILES_TO_CACHE);
+		})
 	);
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', (evt) => {
 	console.log('service worker activated');
 });
-
-// self.addEventListener('fetch', (evt) => {
-// 	evt.respondWith(
-// 		caches.match(evt.request).then((cacheRes) => {
-// 			if (cacheRes) {
-// 				return cacheRes;
-// 			}
-// 		})
-// 	);
-// });
 
 self.addEventListener('fetch', (event) => {
 	if (event.request.url.startsWith(self.location.origin)) {
