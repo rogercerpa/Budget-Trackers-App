f6eeed05dd3695edacc13d92215ab3bdcffe5d9e
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-11 22:02:47 -0400

    updated the service worker with install and fetch info

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,25 @@
+const FILES_TO_CACHE = [ '/', 'index.html', 'style.css', 'index.js' ];
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
+
 self.addEventListener('install', (evt) => {
-	console.log('service worker installed');
+	evt.waitUntil(
+		caches
+			.open(PRECACHE)
+			.then((cache) => cache.addAll(FILES_TO_CACHE))
+			.then(self.skipWaiting())
+	);
 });
 
 self.addEventListener('activate', (evt) => {
 	console.log('service worker activated');
 });
 self.addEventListener('fetch', (evt) => {
-	console.log('fetch event working', evt);
+	evt.respondWith(
+		caches.match(evt.request).then((cacheRes) => {
+			if (cacheRes) {
+				return cacheRes;
+			}
+		})
+	);
 });
