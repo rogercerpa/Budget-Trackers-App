5bcd66bcdafc30ead9a7b5ac9d7c504c9c248473
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-11 23:22:03 -0400

    updated the server, ready for heroku deployment

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,6 +2,8 @@ const FILES_TO_CACHE = [ '/', 'index.html', 'style.css', 'index.js' ];
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
+console.log(FILES_TO_CACHE);
+
 self.addEventListener('install', (evt) => {
 	evt.waitUntil(
 		caches
@@ -14,12 +16,33 @@ self.addEventListener('install', (evt) => {
 self.addEventListener('activate', (evt) => {
 	console.log('service worker activated');
 });
-self.addEventListener('fetch', (evt) => {
-	evt.respondWith(
-		caches.match(evt.request).then((cacheRes) => {
-			if (cacheRes) {
-				return cacheRes;
-			}
-		})
-	);
+
+// self.addEventListener('fetch', (evt) => {
+// 	evt.respondWith(
+// 		caches.match(evt.request).then((cacheRes) => {
+// 			if (cacheRes) {
+// 				return cacheRes;
+// 			}
+// 		})
+// 	);
+// });
+
+self.addEventListener('fetch', (event) => {
+	if (event.request.url.startsWith(self.location.origin)) {
+		event.respondWith(
+			caches.match(event.request).then((cachedResponse) => {
+				if (cachedResponse) {
+					return cachedResponse;
+				}
+
+				return caches.open(RUNTIME).then((cache) => {
+					return fetch(event.request).then((response) => {
+						return cache.put(event.request, response.clone()).then(() => {
+							return response;
+						});
+					});
+				});
+			})
+		);
+	}
 });
