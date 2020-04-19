9e471681a312507ab6f7f9877bee9220938c221f
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-19 15:36:00 -0400

    updated the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,53 +1,49 @@
-const staticCache = [
-	'/',
-	'/index.html',
-	'/style.css',
-	'/index.js',
-	'/app.js'
-];
+const staticCache = [ '/', 'index.html', 'style.css', 'index.js', 'app.js' ];
 const preCache = 'precache-v1';
-// const RUNTIME = 'runtime';
+const RUNTIME = 'runtime';
 
-self.addEventListener('install', (evt) => {
+self.addEventListener('install', function(evt) {
 	evt.waitUntil(
 		caches.open(preCache).then((cache) => {
-			cache.addAll(staticCache);
+			console.log('Your files were pre-cached successfully!');
+			return cache.addAll(staticCache);
 		})
 	);
-	// self.skipWaiting();
+
+	self.skipWaiting();
 });
 
 self.addEventListener('activate', function(evt) {
-	// evt.waitUntil(
-	// 	caches.keys().then((keyList) => {
-	// 		return Promise.all(
-	// 			keyList.map((key) => {
-	// 				if (key !== PRECACHE && key !== RUNTIME) {
-	// 					console.log('Removing old cache data', key);
-	// 					return caches.delete(key);
-	// 				}
-	// 			})
-	// 		);
-	// 	})
-	// );
-	// self.clients.claim();
+	evt.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(
+				keyList.map((key) => {
+					if (key !== PRECACHE && key !== RUNTIME) {
+						console.log('Removing old cache data', key);
+						return caches.delete(key);
+					}
+				})
+			);
+		})
+	);
+	self.clients.claim();
 });
 
 self.addEventListener('fetch', (evt) => {
-	// if (evt.request.url.startsWith(self.location.origin)) {
-	// 	evt.respondWith(
-	// 		caches.match(evt.request).then((cachedResponse) => {
-	// 			if (cachedResponse) {
-	// 				return cachedResponse;
-	// 			}
-	// 			return caches.open(RUNTIME).then((cache) => {
-	// 				return fetch(evt.request).then((response) => {
-	// 					return cache.put(evt.request, response.clone()).then(() => {
-	// 						return response;
-	// 					});
-	// 				});
-	// 			});
-	// 		})
-	// 	);
-	// }
+	if (evt.request.url.startsWith(self.location.origin)) {
+		evt.respondWith(
+			caches.match(evt.request).then((cachedResponse) => {
+				if (cachedResponse) {
+					return cachedResponse;
+				}
+				return caches.open(RUNTIME).then((cache) => {
+					return fetch(evt.request).then((response) => {
+						return cache.put(evt.request, response.clone()).then(() => {
+							return response;
+						});
+					});
+				});
+			})
+		);
+	}
 });
