ed1c171dd2c16565486a47ab796d5f51dc798ff1
Roger Cerpa <roger.e.cerpa@gmail.com>
2020-04-19 15:03:36 -0400

    updated the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,10 @@
-const FILES_TO_CACHE = [ '/', '/index.html', '/style.css', '/index.js' ];
+const FILES_TO_CACHE = [
+	'/',
+	'/index.html',
+	'/style.css',
+	'/index.js',
+	'/app.js'
+];
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 
@@ -8,42 +14,40 @@ self.addEventListener('install', (evt) => {
 			cache.addAll(FILES_TO_CACHE);
 		})
 	);
-	self.skipWaiting();
+	// self.skipWaiting();
 });
 
 self.addEventListener('activate', function(evt) {
-	evt.waitUntil(
-		caches.keys().then((keyList) => {
-			return Promise.all(
-				keyList.map((key) => {
-					if (key !== PRECACHE && key !== RUNTIME) {
-						console.log('Removing old cache data', key);
-						return caches.delete(key);
-					}
-				})
-			);
-		})
-	);
-
-	self.clients.claim();
+	// evt.waitUntil(
+	// 	caches.keys().then((keyList) => {
+	// 		return Promise.all(
+	// 			keyList.map((key) => {
+	// 				if (key !== PRECACHE && key !== RUNTIME) {
+	// 					console.log('Removing old cache data', key);
+	// 					return caches.delete(key);
+	// 				}
+	// 			})
+	// 		);
+	// 	})
+	// );
+	// self.clients.claim();
 });
 
-self.addEventListener('fetch', (event) => {
-	if (event.request.url.startsWith(self.location.origin)) {
-		event.respondWith(
-			caches.match(event.request).then((cachedResponse) => {
-				if (cachedResponse) {
-					return cachedResponse;
-				}
-
-				return caches.open(RUNTIME).then((cache) => {
-					return fetch(event.request).then((response) => {
-						return cache.put(event.request, response.clone()).then(() => {
-							return response;
-						});
-					});
-				});
-			})
-		);
-	}
+self.addEventListener('fetch', (evt) => {
+	// if (evt.request.url.startsWith(self.location.origin)) {
+	// 	evt.respondWith(
+	// 		caches.match(evt.request).then((cachedResponse) => {
+	// 			if (cachedResponse) {
+	// 				return cachedResponse;
+	// 			}
+	// 			return caches.open(RUNTIME).then((cache) => {
+	// 				return fetch(evt.request).then((response) => {
+	// 					return cache.put(evt.request, response.clone()).then(() => {
+	// 						return response;
+	// 					});
+	// 				});
+	// 			});
+	// 		})
+	// 	);
+	// }
 });
